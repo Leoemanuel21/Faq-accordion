255c657edfa17c2d9a51da19ec20953ba59d3d5c
Leoemanuel21 <leoemanuel14.rocha@gmail.com>
2025-07-22 11:26:59 -0300

    script concluído

--- script.js
+++ script.js
@@ -1,22 +1,29 @@
-function mudouTamanho(){
-    if(window.innerwidth >= 768){
-        itens.style.display = 'block'   
-    }else{
-            itens.style.display = 'none'
-        }
+for(let i=1; i<=4; i++){
+    let plus = document.getElementById(`plus${i}`).style.display = 'block';
+    let minus = document.getElementById(`minus${i}`).style.display = 'none';
+    let p = document.getElementById(`p${i}`).style.display = 'none';
 }
-function clickMenu(){
-    if(itens.style.display == 'block'){
-        itens.style.display = 'none'
-    }else{
-        itens.style.display = 'block'
+
+function response(i) {
+    let plus = document.getElementById(`plus${i}`);
+    let minus = document.getElementById(`minus${i}`);
+    let p = document.getElementById(`p${i}`);
+    
+    if (plus.style.display === 'block') {
+        plus.style.display = 'none';
+        minus.style.display = 'block';
+        if (p) p.style.display = 'block';
+    } else {
+        plus.style.display = 'block';
+        minus.style.display = 'none';
+        if (p) p.style.display = 'none';
     }
 }
 
-function resposta(){
-    if(itens.style.display == 'block'){
-        itens.style.display = 'none'
-    }else{
-        itens.style.display = 'block'
+function reset(){
+    for(let i=1; i<=4; i++){
+    let plus = document.getElementById(`plus${i}`).style.display = 'block';
+    let minus = document.getElementById(`minus${i}`).style.display = 'none';
+    let p = document.getElementById(`p${i}`).style.display = 'none';
     }
 }
